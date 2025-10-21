6d7522741ba53c756c6db6bb91587c0049bb5da6
pinyopapi <95istvan@gmail.com>
2025-10-21 16:36:07 +0200

    Create terraform main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.eks_subnets[*].id
+  }
+}
+
+resource "aws_iam_role" "eks_role" {
+  name = "eksRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Egyszerű példa subnetekre
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "eks_subnets" {
+  count             = 2
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+}
+
+data "aws_availability_zones" "available" {}
