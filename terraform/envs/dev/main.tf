f2cc61ce303a99b8b84bae8c4024082d5d555bf0
pinyopapi <95istvan@gmail.com>
2025-11-06 14:09:15 +0100

    Add vpc

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -4,9 +4,16 @@ module "s3" {
 }
 
 module "rds" {
-  source          = "../../modules/rds"
-  db_name         = "devdb"
-  username        = "devuser"
-  password        = "devpass"
+  source            = "../../modules/rds"
+  db_name           = "devdb"
+  username          = "devuser"
+  password          = "devpass"
   allocated_storage = 20
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.subnet_ids
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
 }
