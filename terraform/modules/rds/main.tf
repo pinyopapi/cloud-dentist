512a2b73bd95cb43b557925fdedcc402f5f8d332
pinyopapi <95istvan@gmail.com>
2025-11-06 14:03:07 +0100

    Expand rds with security group

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -1,3 +1,28 @@
+resource "aws_db_subnet_group" "this" {
+  name       = "${var.db_name}-subnet-group"
+  subnet_ids = var.subnet_ids
+}
+
+resource "aws_security_group" "this" {
+  name        = "${var.db_name}-sg"
+  description = "Allow PostgreSQL traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_db_instance" "this" {
   allocated_storage    = var.allocated_storage
   engine               = "postgres"
@@ -6,10 +31,8 @@ resource "aws_db_instance" "this" {
   name                 = var.db_name
   username             = var.username
   password             = var.password
+  db_subnet_group_name = aws_db_subnet_group.this.name
+  vpc_security_group_ids = [aws_security_group.this.id]
   skip_final_snapshot  = true
   publicly_accessible  = false
-}
-
-output "endpoint" {
-  value = aws_db_instance.this.endpoint
 }
--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -4,3 +4,5 @@ variable "password" { type = string }
 variable "allocated_storage" { type = number, default = 20 }
 variable "engine_version" { type = string, default = "15.3" }
 variable "instance_class" { type = string, default = "db.t3.micro" }
+variable "vpc_id" { type = string }
+variable "subnet_ids" { type = list(string) }
